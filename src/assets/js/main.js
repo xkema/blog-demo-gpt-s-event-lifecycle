bfd4ae6c7eeee367d9cfc54d14de2852555be05f
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-08-15 16:28:29 +0300

    Redefined timeline styles with flex

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
-  document.querySelector('.grid-graph').insertAdjacentElement('afterBegin', demotag.slots['div-gpt-ad-1596659457894-0'].timelineElement);
-  document.querySelector('.grid-graph').insertAdjacentElement('afterBegin', demotag.slots['div-gpt-ad-9405734-1'].timelineElement);
+  document.querySelector('.grid-graph .timelines').insertAdjacentElement('beforeEnd', demotag.slots['div-gpt-ad-9405734-1'].timelineElement);
+  document.querySelector('.grid-graph .timelines').insertAdjacentElement('beforeEnd', demotag.slots['div-gpt-ad-1596659457894-0'].timelineElement);
 });
 
 // a global object to enclose page data
@@ -32,7 +32,10 @@ const demotag = ((window, document, googletag) => {
    */
   const updateTimelineElement = (currentEvent, timelineElement) => {
     // create timeline item and item header for each event if it is not created before
-    let timelineItem = timelineElement.querySelector(`.${currentEvent.type}`);
+    // first child of "timelineItemsElement" is ".timeline-iddle.start" element, last is ".timeline-iddle.end" element
+    const timelineItemsElement = timelineElement.firstElementChild;
+    let timelineItem = timelineItemsElement.querySelector(`.timeline-item.${currentEvent.type}`);
+    let timelineIddle = timelineItemsElement.querySelector(`.timeline-iddle.${currentEvent.type}`);
     if(null === timelineItem) {
       // create timeline item for current event
       timelineItem = document.createElement('div');
@@ -40,33 +43,51 @@ const demotag = ((window, document, googletag) => {
       // add a color visualizer and header to timeline item
       timelineItem.insertAdjacentHTML('afterBegin', `
         <span class="timeline-timing">&nbsp;</span>
-        <span class="timeline-separator">&laquo;</span>
         <span class="timeline-label">${currentEvent.type}</span>
       `);
+      // add iddle timing indicator
+      timelineIddle = document.createElement('div');
+      timelineIddle.classList.add('timeline-iddle', `${currentEvent.type}`);
+      // add a color visualizer to timeline iddle item
+      timelineIddle.insertAdjacentHTML('afterBegin', `
+        <span class="timeline-iddle-timing">${currentEvent.timingDiff}ms</span>
+      `);
+      // insert timeline iddle item to the timeline element
+      timelineItemsElement.lastElementChild.insertAdjacentElement('beforeBegin', timelineIddle);
       // insert timeline item to the timeline element
-      timelineElement.insertAdjacentElement('beforeEnd', timelineItem);
+      timelineItemsElement.lastElementChild.insertAdjacentElement('beforeBegin', timelineItem);
     }
     // update percentages if event is visibility change event
     if('slotVisibilityChanged' === currentEvent.type) {
-      const timelineDetailsElement = timelineElement.querySelector('.timeline-details')
+      const timelineDetailsElement = timelineItemsElement.querySelector('.timeline-details')
       if(null === timelineDetailsElement) {
         timelineItem.insertAdjacentHTML('beforeEnd', `
-          <span class="timeline-separator">|</span>
           <span class="timeline-details">${currentEvent.inViewPercentage}%</span>`);
       } else {
         timelineDetailsElement.textContent = `${currentEvent.inViewPercentage}%`;
       }
     }
-    // translate headers to visualize event timings
-    let timingMargin = Math.floor(currentEvent.timingDiff / 2);
-    if(timingMargin > 100) {
-      timelineItem.classList.add('timeline-iddle');
-      const previousItemPadding = parseInt(timelineItem.previousElementSibling.style.marginLeft);
-      timingMargin = previousItemPadding + 100;
+    // set min/max for timing margin
+    let previousIddleMargin = 0;
+    if(timelineIddle.previousElementSibling && timelineIddle.previousElementSibling.previousElementSibling) {
+      previousIddleMargin = parseInt(timelineIddle.previousElementSibling.previousElementSibling.style.flexGrow);
     }
-    // update left margins with preventing multiple updates for visibility change events
-    if(timelineItem.style.marginLeft === "") {
-      timelineItem.style.marginLeft = `${timingMargin}px`;
+    // restrict max iddle time duration 2s and set max flexGrow to double of previous margin for durations longer than 2s
+    // restrict min iddle time margin to 200
+    let iddleMargin = currentEvent.timingDiff;
+    if(iddleMargin > 2000) {
+      iddleMargin = previousIddleMargin * 2;
+    }
+    if(iddleMargin < 200) {
+      iddleMargin = 200;
+    }
+    // ensure single time updates for "slotVisibilityChanged" events
+    if(timelineIddle.style.flexGrow === '') {
+      timelineIddle.style.flexGrow = `${iddleMargin}`;
+    }
+    // seal timeline element with finish string
+    if(null !== timelineItemsElement.lastElementChild && 'impressionViewable' === currentEvent.type) {
+      timelineItemsElement.lastElementChild.firstElementChild.innerHTML = 'finish';
     }
   };
 
@@ -78,6 +99,15 @@ const demotag = ((window, document, googletag) => {
   const createTimelineElement = (currentSlotId='unknown-slot-id') => {
     const element = document.createElement('div');
     element.classList.add('timeline-container', `demotag-${currentSlotId}`);
+    element.insertAdjacentHTML('afterBegin', `
+      <div class="timeline-items">
+        <div class="timeline-iddle start">
+          <span class="timeline-iddle-timing">start</span>
+        </div>
+        <div class="timeline-iddle finish">
+          <span class="timeline-iddle-timing">waiting-next-event</span>
+        </div>
+      </div>`);
     return element;
   };
 
@@ -129,7 +159,7 @@ const demotag = ((window, document, googletag) => {
   };
 })(window, document, googletag);
 
-console.log(`%cdebug ::`, `color:crimson;font-weight:bold;`, demotag);
+// console.log(`%cdebug ::`, `color:crimson;font-weight:bold;`, demotag);
 
 // gpt definitions
 googletag.cmd.push(function() {
